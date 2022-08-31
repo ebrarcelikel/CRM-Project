6836d5631503898f0f294c5beb384eb923fe3a26
ZEYNEP EBRAR ÇELİKEL <ebrar.celikell@gmail.com>
2022-08-31 23:22:03 +0300

    4 changes

---
+++ CrmProject.DataAccess/Migrations/20220831201058_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrmProject.DataAccess.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fullname = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Explanation = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Locked = table.Column<bool>(type: "bit", nullable: false),
+                    IsCorporate = table.Column<bool>(type: "bit", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
--- CrmProject.Models/CustomerModels.cs
+++ CrmProject.Models/CustomerModels.cs
@@ -5,9 +5,7 @@
         public string Fullname { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-
         public string Explanation { get; set; }
-
         public bool Locked { get; set; }
         public bool IsCorporate { get; set; }
 
